629a3f3ad3e720f3615d55620debf7ab43688637
charlesoliveiracastro <charlesoliveiracastro@gmail.com>
2025-05-20 22:10:13 -0300

    iniciando state

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
+import { TaskStateModel } from './models/TaskStateModel';
 import { Home } from './pages/home';
 
 import './styles/global.css';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
   return <Home />;
